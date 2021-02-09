4e68a4e025bbacaa84c243ee2ebffcc20355f37e
Deepesh Padala <deepesh.padala02@gmail.com>
2021-02-10 01:12:31 +0530

    Initial commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/d33p/SLAM_research/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/d33p/catkin_ws/devel;/home/deep/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/d33p/catkin_ws/devel;/home/deep/catkin_ws/devel;/opt/ros/melodic' != '' else []
