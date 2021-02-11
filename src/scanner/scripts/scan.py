e4286458c4a0073aef02e6a54a3aa04b4ef99478
Deepesh Padala <deepesh.padala02@gmail.com>
2021-02-11 21:33:31 +0530

    Latest commit, scan.py working for turtle, but not for the bot

--- src/scanner/scripts/scan.py
+++ src/scanner/scripts/scan.py
@@ -17,26 +17,30 @@ def radial_scan():
         We need the bot to move in a spiral, so I'll just keep the angular velocity constant
         And accelerate the linear velocity, so as to increase the radius of rotation
         so, we can find the velocity using the formula v = u + at
+
+        The angular velocity z doesn't add any linear velocity, as the axis of rotation
+        is about the centre of mass of the robot, which just makes it turn, 
+        and not give any linear velocity at all
     """
-    radius_to_scan = 0.3 # in metres, or whatever units are used in the gazebo thingy
+
+    radius_to_scan = 2 # in metres, or whatever units are used in the gazebo thingy
     angular_speed = 2*pi # 1 rotation per second
 
-    # 0.25 rotation per second should be, but it's taking 23 seconds for one rotation, instead of 4
-    time_to_complete = 10 # seconds
-    time_at_start = time.time()
+    time_to_complete = 20 # seconds
 
     v_initial = 0
-    v_final = 2 * pi * radius_to_scan * angular_speed
+    v_final = 2 * pi * radius_to_scan * angular_speed / (2*pi) 
+    # angular_speed / angle covered in one rotation = 1 / time_taken
+    # and, total distance travelled = 2 * pi * r in the final circle, so
+    # v_final = 2*pi*r*(angular_speed/2*pi)
 
     acceleration = (v_final - v_initial)/time_to_complete
     print("The acceleration is : ", acceleration)
 
     v_current = 0
     vel_msg.angular.z = angular_speed
-#    v = 2*pi*radius_to_scan*angular_speed
-#    vel_msg.linear.x = v
 
-
+    time_at_start = time.time()
     while v_current <= v_final:
         v_current = v_initial + acceleration*(time.time()-time_at_start)
         if int(time.time()-time_at_start) % 2 == 0:
@@ -62,6 +66,6 @@ def turn_90():
 
 if __name__ == "__main__":
 #    while not rospy.is_shutdown():
-#    radial_scan()
-    turn_90()
+    radial_scan()
+#    turn_90()
     exit(0)
