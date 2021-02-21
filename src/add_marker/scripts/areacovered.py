a8a4d1634aa68de4e62f7114e438702840e363a2
gautampa7 <gautampa7@gmail.com>
2021-02-21 19:03:45 +0530

    Fixed Publisher issue with area mapper node

--- src/add_marker/scripts/areacovered.py
+++ src/add_marker/scripts/areacovered.py
@@ -3,14 +3,22 @@
 import roslib
 import rospy
 from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Int32
 from nav_msgs.msg import OccupancyGrid
 import tf
 
 rospy.init_node("MapAreaGraph", anonymous = True)
+
+topic = "map_covered"    
+publisher = rospy.Publisher(topic, Int32,queue_size=10) 
+
 def area_covered(data):
     mapdata=data.data
+    totalcells=len(mapdata)
     non_covered_cells= mapdata.count(-1)
-    print(non_covered_cells)
+    area_covered_points=totalcells-non_covered_cells
+    publisher.publish(area_covered_points)
+    print(str(area_covered_points) + "/" + str(totalcells))
     
 
 def graph_area():
